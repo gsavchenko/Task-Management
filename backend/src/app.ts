c6adee8ccd581408353e7c9253b3ba8400916560
George Savchenko <george.savchenko@gmail.com>
2024-08-11 16:06:43 -0400

    backend - allow frontend in cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import taskRoutes from "./routes/taskRoutes";
 import errorHandler from "./middleware/errorHandler";
+import cors from "cors";
+
+const corsOptions = {
+  origin: "http://localhost:5173", // TODO: DO NOT HARDCODE FOR PRODUCTION
+};
 
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json());
-
 app.use("/api/tasks", taskRoutes);
-
 app.use(errorHandler);
 
 export default app;
